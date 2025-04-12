c50a09fb3f8cfe92375e0614374a9fa5389d6486
ppoliakov <pasha.u.polyak@gmail.com>
2025-04-12 23:17:54 +0300

    initial commit

---
+++ sources/main.cpp
@@ -0,0 +1,18 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window{sf::VideoMode({800u, 600u}), "CMake SFML Project"};
+    window.setFramerateLimit(144);
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+    return 0;
+}
