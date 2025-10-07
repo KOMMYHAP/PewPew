5a6affed58216adb2284991c4f9513b1cf93a42c
ppoliakov <pasha.u.polyak@gmail.com>
2025-10-08 00:27:46 +0300

    [PEW-1] resource graph (wip)

--- sources/resources/resource_registry.cpp
+++ sources/resources/resource_registry.cpp
@@ -25,8 +25,24 @@ ResourceNodeIterator& ResourceNodeIterator::operator++()
 ResourceNodeIterator ResourceNodeIterator::operator++(int)
 {
   const ResourceNodeIterator tmp = *this;
-  ++(*this);
+  ++*this;
   return tmp;
+}
+
+ResourceNode::ResourceNode(const ResourceNode* parent, std::filesystem::path name, std::filesystem::path path)
+  : _name(std::move(name))
+  , _path(std::move(path))
+  , _parent(parent)
+{
+}
+
+ResourceNode& ResourceNode::AddChild(std::filesystem::path name, std::filesystem::path path)
+{
+  assert(name.has_filename());
+  assert(!path.has_parent_path());
+  _children.push_back(std::make_unique<ResourceNode>(this, std::move(name), std::move(path)));
+  const std::unique_ptr<ResourceNode>& child = _children.back();
+  return *child;
 }
 
 bool operator==(const ResourceNodeIterator& a, const ResourceNodeIterator& b)
@@ -39,24 +55,47 @@ bool operator!=(const ResourceNodeIterator& a, const ResourceNodeIterator& b)
   return !(a == b);
 }
 
-void ResourceGraph::BreadthFirstSearch(Visitor visitor) const
+ResourceGraph::ResourceGraph(const std::filesystem::path& graphPath)
+: _root(std::make_unique<ResourceNode>(nullptr, graphPath.parent_path(), graphPath.filename()))
 {
-  std::vector<const ResourceNode*> nodes;
+}
 
+void ResourceGraph::VisitBreadthFirst(Visitor visitor) const
+{
   std::queue<const ResourceNode*> nodesToVisit;
   nodesToVisit.push(_root.get());
   while (!nodesToVisit.empty()) {
     const ResourceNode* node = nodesToVisit.front();
     nodesToVisit.pop();
 
-    nodes.push_back(node);
+    visitor(*node);
 
-    for (const ResourceNode & child : *node) {
+    for (const ResourceNode& child : *node) {
       nodesToVisit.push(&child);
     }
   }
+}
 
-  for (const ResourceNode* node : std::ranges::reverse_view(nodes)) {
-    visitor(*node);
+ResourceNode& ResourceGraph::AddChild(const std::filesystem::path& path, std::filesystem::path name)
+{
+  std::filesystem::relative(_root, path, );
+}
+
+std::filesystem::path ResourceGraph::MakePathToNode(const std::filesystem::path& root, const ResourceNode& node)
+{
+  std::filesystem::path path = root;
+
+  std::vector<const ResourceNode*> hierarchy;
+  const ResourceNode* entry = &node;
+  hierarchy.push_back(entry);
+  while (entry->HasParent()) {
+    entry = &entry->GetParent();
+    hierarchy.push_back(entry);
   }
+
+  for (const ResourceNode* parent : std::ranges::reverse_view(hierarchy)) {
+    path /= parent->GetPath();
+  }
+
+  return path;
 }
--- sources/resources/resource_registry.h
+++ sources/resources/resource_registry.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "resource_desc.h"
 
 class DescRegistry;
 
@@ -38,12 +39,14 @@ class ResourceNode
 {
   friend struct ResourceNodeIterator;
 public:
-  ResourceNode(std::string name, std::string path);
+  ResourceNode(const ResourceNode * parent, std::filesystem::path name, std::filesystem::path path);
 
-  std::string_view GetName() const { return _name; }
-  std::string_view GetPath() const { return _path; }
+  const std::filesystem::path& GetName() const { return _name; }
+  const std::filesystem::path& GetPath() const { return _path; }
+  const ResourceNode& GetParent() const { return *_parent; }
+  bool HasParent() const { return _parent != nullptr; }
 
-  void AddChild(std::string name, std::string path);
+  ResourceNode & AddChild(std::filesystem::path name, std::filesystem::path path);
 
   using Iterator = const ResourceNode*;
 
@@ -51,26 +54,44 @@ public:
   ResourceNodeIterator end() const { return ResourceNodeIterator(*this, _children.size()); }
 
 private:
-  std::string _name;
-  std::string _path; //< relative to the parent
+  std::filesystem::path _path; //< relative to the parent
   std::vector<std::unique_ptr<ResourceNode>> _children;
+  const ResourceNode* _parent{nullptr};
+};
+
+enum class ResourceNodeId : uint16_t
+{
+  Invalid = std::numeric_limits<uint16_t>::max()
+};
+
+class ResourceNodeLink
+{
+  ResourceNodeId parent{ResourceNodeId::Invalid};
+  ResourceNodeId node{ResourceNodeId::Invalid};
+  std::vector<ResourceNodeId> children;
 };
 
 class ResourceGraph
 {
 public:
+  ResourceGraph(const std::filesystem::path& graphPath);
+
   enum class VisitorStep
   {
     Continue,
     Stop
   };
   using Visitor = std::move_only_function<VisitorStep(const ResourceNode& /*node*/)>;
-  void BreadthFirstSearch(Visitor visitor) const;
+  void VisitBreadthFirst(Visitor visitor) const;
 
+  ResourceNode & AddChild(const std::filesystem::path& path, std::filesystem::path name);
+
+  static std::filesystem::path MakePathToNode(const std::filesystem::path &root, const ResourceNode& node);
 
 private:
-  std::unique_ptr<ResourceNode> _root;
-  std::string _rootPath;
+  std::vector<ResourceNode> _nodes;
+  std::unordered_map<std::filesystem::path, ResourceNodeId> _indexNodesByPath;
+  std::vector<ResourceNodeLink> _dependenciesGraph;
 };
 
 class ResourcesRegistry
